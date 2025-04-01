e72bab05112e9480ff30c3d42d71069a118cfd7e
Perdomo24 <rperdomo32@gmail.com>
2025-04-01 13:30:41 -0600

    Agregando mud blazor para la UI template

--- MoneyTracker.UI/Program.cs
+++ MoneyTracker.UI/Program.cs
@@ -2,6 +2,8 @@ using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using MoneyTracker.Infrastructure.Persistence;
 using MoneyTracker.UI.Components;
+using MudBlazor;
+using MudBlazor.Services;
 using Serilog;
 
 namespace MoneyTracker.UI
@@ -12,9 +14,14 @@ namespace MoneyTracker.UI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+          
+
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+            });
 
             builder.Services.AddDbContext<MoneyTrackerDbContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -25,6 +32,10 @@ namespace MoneyTracker.UI
                             .WriteTo.Console()
                             .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
                             .CreateLogger();
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
             var app = builder.Build();
 
