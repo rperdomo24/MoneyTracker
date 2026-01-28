cd8ae739def3c82098c20d366a3bf6bf8c0cbe4f
Perdomo24 <rperdomo32@gmail.com>
2026-01-28 06:23:58 -0600

    Migracion pendiente

---
+++ MoneyTracker.Infrastructure/Migrations/20260116050112_FixPendingModelChanges.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransferType",
+                table: "Transaction");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Type",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#4CAF50", 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#2196F3", 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Type",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#FF5722", 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#4CAF50", 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TransferType",
+                table: "Transaction",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Type",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#FF9800", 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#4CAF50", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Type",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#F44336", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Color", "Type" },
+                values: new object[] { "#F44336", 1 });
+        }
+    }
+}
